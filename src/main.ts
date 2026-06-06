b9636969d5d96a8215763867814c68111e6b65f2
chandhru <chandhru.ravi@bunch.capital>
2026-06-07 01:20:43 +0200

    feat: add postLogoutRedirectUri config

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ app.use(createAuthAction({
   domain: import.meta.env.VITE_AUTHACTION_DOMAIN,
   clientId: import.meta.env.VITE_AUTHACTION_CLIENT_ID,
   redirectUri: import.meta.env.VITE_AUTHACTION_REDIRECT_URI,
+  postLogoutRedirectUri: import.meta.env.VITE_AUTHACTION_LOGOUT_REDIRECT_URI,
 }))
 
 app.mount('#app')
